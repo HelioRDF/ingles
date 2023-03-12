773103ae2188bddb0c314bef80a703cf16d92938
Helio Franca <heliordf@hotmail.com>
2023-03-12 12:26:41 -0300

    Alfabeto!

--- javascript/verbo.js
+++ javascript/verbo.js
@@ -30,7 +30,7 @@ function addTable(palavras, id) {
     let palavraPortugues = palavras.palavraPortugues;
     let divAudio = $("#audio")
     let audio = $("<audio>").attr("id", palavraIngles).attr("class",
-        "play").attr("src", `./audio/${palavraIngles
+        "play").attr("src", `./audio/verbo/${palavraIngles
             }.mp3`)
     divAudio.append(audio)
     let trTable = $("<tr>");
