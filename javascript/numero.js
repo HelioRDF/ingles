de476d52bf8f7b1bb8c511ce58de3f45ae6aec50
Helio Franca <heliordf@hotmail.com>
2023-03-12 12:37:20 -0300

    Alfabeto!

---
+++ javascript/numero.js
@@ -0,0 +1,84 @@
+$(() => {
+    let id = 1
+    numero.forEach(element => {
+        addTable(element, id, "numero")
+        id++;
+    });
+
+    let audioControl = document.getElementsByClassName("play")
+    for (i = 0; i < audioControl.length; i++) {
+        audioControl[i
+        ].playbackRate = speedControl.value;
+    }
+})
+
+function playAudio(id) {
+    const audio = document.getElementById(id);
+    audio.play();
+}
+
+const speedControl = document.getElementById('speed');
+function changeSpeed() {
+    let audioControl = document.getElementsByClassName("play")
+    for (i = 0; i < audioControl.length; i++) {
+        audioControl[i
+        ].playbackRate = speedControl.value;
+    }
+}
+
+function addTable(palavras, id, table) {
+    let palavraIngles = palavras.palavraIngles;
+    let palavraPortugues = palavras.palavraPortugues;
+    let divAudio = $("#audio")
+    let audio = $("<audio>").attr("id", palavraIngles).attr("class",
+        "play").attr("src", `./audio/numero/${palavraIngles
+            }.mp3`)
+    divAudio.append(audio)
+    let trTable = $("<tr>");
+    let thTableId = $("<th>").attr("scope",
+        "row").text(id);
+    let iconePlay = $("<i>").attr("class",
+        "fas fa-play material-icons")
+    let button = $("<button>").attr("type",
+        "button").attr("class",
+            "play-button").attr("onclick", `playAudio('${palavraIngles
+                }')`).append(iconePlay);;
+    let tdTableButton = $("<td>").attr("scope",
+        "row").append(button);
+    let tdTablePalavra = $("<td>").attr("scope",
+        "row").text(palavraIngles.toUpperCase());
+    let tdTabletTraducao = $("<td>").attr("scope",
+        "row").text(palavraPortugues.toUpperCase());
+
+
+
+    trTable.append(thTableId);
+    trTable.append(tdTableButton);
+
+    trTable.append(tdTablePalavra);
+    trTable.append(tdTabletTraducao);
+    table = $("#" + table);
+    table.append(trTable);
+}
+
+const numero =
+    [
+        {
+            palavraIngles: "one",
+            palavraPortugues: "1",
+        },
+        {
+            palavraIngles: "two",
+            palavraPortugues: "2",
+        }, {
+            palavraIngles: "three",
+            palavraPortugues: "3",
+        }, {
+            palavraIngles: "four",
+            palavraPortugues: "4",
+        }, {
+            palavraIngles: "five",
+            palavraPortugues: "5",
+        },
+    ]
+
